061b552bba4736baa64e22adbc835f33e5619172
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-03 09:32:59 -0300

    teste de commit para o github

---
+++ jselenium/src/main/java/com/jsoaresme/teste/Teste002.java
@@ -0,0 +1,14 @@
+package com.jsoaresme.teste;
+
+public class Teste002 {
+	
+	private static String texto;
+	
+	public static String getTexto() {
+		return texto;
+	}
+
+	public static void setTexto(String texto) {
+		Teste002.texto = texto;
+	}
+}
