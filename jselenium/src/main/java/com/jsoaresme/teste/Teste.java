85c02ac175c9ffa99a46b35cadd31ecf3fc76e7c
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-08 16:26:25 -0300

    Commitando projeto com o teste rodando

--- jselenium/src/main/java/com/jsoaresme/teste/Teste.java
+++ jselenium/src/main/java/com/jsoaresme/teste/Teste.java
@@ -1,11 +1,14 @@
 package com.jsoaresme.teste;
 
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class Teste {
 
 	public static void main(String[] args) {
 		System.out.println("Teste");
 		System.out.println("");
 		System.out.println("Teste03");
+		FirefoxDriver driver = new FirefoxDriver();
 
 	}
 
