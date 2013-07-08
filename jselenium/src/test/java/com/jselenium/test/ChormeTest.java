a7db0a96e1eca6f1a48e266178c6e227d88e8c4f
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-08 16:32:25 -0300

    Teste rodando pelo maven, teste googlechrome

--- jselenium/src/test/java/com/jselenium/test/ChormeTest.java
+++ jselenium/src/test/java/com/jselenium/test/ChormeTest.java
@@ -2,21 +2,16 @@ package com.jselenium.test;
 
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 
 public class ChormeTest {
 
-//	ChromeDriver driver = new ChromeDriver();
-	FirefoxDriver driver = new FirefoxDriver();
-	
 	@Test
 	public void openWebChrome() {
 		String url;
 		url = "https://www.google.com.br/";
 		System.out.println("\n" + "- Open Chrome");
-		//System.setProperty("webdriver.chrome.driver", "\\chromedriver.exe");
-		
+		System.setProperty("webdriver.chrome.driver", "\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
 		driver.get(url);
 		System.out.println("Test funcionou");
 		
