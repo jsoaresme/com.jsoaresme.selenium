ed1885ca2bdc0f3ba4dad49170bcc35bb1a3672f
jonathanSoares <jonathanSoares@172.29.2.27>
2013-07-08 16:15:26 -0300

    adicionando o teste do selenium

--- jselenium/src/test/java/com/jselenium/test/ChormeTest.java
+++ jselenium/src/test/java/com/jselenium/test/ChormeTest.java
@@ -1,20 +1,24 @@
-package com.jselenium.chorme;
+package com.jselenium.test;
 
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
-public class OpenChromeTest {
+
+public class ChormeTest {
+
+//	ChromeDriver driver = new ChromeDriver();
+	FirefoxDriver driver = new FirefoxDriver();
 	
 	@Test
 	public void openWebChrome() {
 		String url;
 		url = "https://www.google.com.br/";
 		System.out.println("\n" + "- Open Chrome");
-		System.setProperty("webdriver.chrome.driver", "\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "\\chromedriver.exe");
+		
 		driver.get(url);
 		System.out.println("Test funcionou");
 		
 	}
-
 }
